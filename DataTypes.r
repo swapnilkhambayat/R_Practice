72ad45d0d0e941b66fc33cb7e0f9149b71fd2933
Swapnil Khambayat <38365464+swapnilkhambayat@users.noreply.github.com>
2020-08-16 11:02:35 +1000

    15August2020

---
+++ DataTypes.r
@@ -0,0 +1,19 @@
+#String
+a <- "h"
+typeof(a)
+
+#integer
+n <- 2L
+typeof(n)
+
+#Double
+d <- 4
+typeof(d)
+
+#complex
+c <- 3 + 2i
+typeof(c)
+
+#logical (boolean) T or F  or TRUE or FALSE
+l <- TRUE
+typeof(l)
---
+++ String and numral operations.r
@@ -0,0 +1,22 @@
+A <- 10
+B <- 5
+
+C <- A + B # spaces dont matter
+
+C
+
+#variable 1
+var1 <- 2.5
+#variable 2
+var2 <- 4
+
+result <- var1 * var2
+result
+
+#Square root function 
+answer <- sqrt(var2)
+
+#string operation
+greeting <- "Hello"
+name <- "Bob"
+message <- paste(greeting,name,"!!!")
