4a5d584cb62b475337c50ce80992f4bc8425720d
Swapnil Khambayat <38365464+swapnilkhambayat@users.noreply.github.com>
2020-08-16 20:59:46 +1000

    16August2020

---
+++ Logical Variables and Operators.r
@@ -0,0 +1,33 @@
+#Logical operator 
+10 > 100
+
+4 == 5
+
+# Types of Operator as below:
+# ==  equal to 
+# !=  Not equal to 
+# <   less than
+# >   greater than
+# <=  less than equal to 
+# >=  greater than equal to
+# !   not operator
+# |   Or
+# &   And
+# isTRUE(x)
+
+logic <-  4 < 5
+logic
+typeof(logic)
+
+#check not operator
+notope <- !(5>1)
+notope
+
+#Check OR operator
+logic | notope
+
+#Check AND operator
+logic & notope
+
+#Check isTRUE operator
+isTRUE(logic)
